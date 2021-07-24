dbfe7d0901174f4108734bdd50d118d1007cc04d
DariusCristianGligor <darius.gligor00@e-uvt.ro>
2021-07-24 18:15:44 +0300

    asdasd

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -13,6 +13,9 @@ namespace ConsoleApp1
             Console.WriteLine("HEii");
             Console.WriteLine("asdasd");
             Console.WriteLine("HEii2222222222");
+            Console.WriteLine("HEii2222222222");
+            Console.WriteLine("HEii2222222222");
+            Console.WriteLine("HEii2222222222");
         }
     }
 }
