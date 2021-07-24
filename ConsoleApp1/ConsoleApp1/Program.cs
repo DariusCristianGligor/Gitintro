1a6d8e3619cada8080c28a109eb0998b894313b8
DariusCristianGligor <darius.gligor00@e-uvt.ro>
2021-07-24 18:37:25 +0300

    123

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -14,7 +14,7 @@ namespace ConsoleApp1
             Console.WriteLine("asdasd");
             Console.WriteLine("HEii2222222222");
             Console.WriteLine("HEii22222222asdasdasd22222222");
-            Console.WriteLine("HEii22222sssssssssssssssssssssssssssssss22222");
+            Console.WriteLine("HEii22222ssssssssssssss2222222222sssssssssssssssss22222");
             Console.WriteLine("HEii2222222222"1111111111);
         }
     }
