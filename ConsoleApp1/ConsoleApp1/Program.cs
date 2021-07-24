505e4c2ea16160084f9b65e6b2d80f6a455254c2
DariusGligor <darius.gligor00@e-uvt.ro>
2021-07-24 13:24:47 +0300

    primul schimba

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("HEii");
-            Console.WriteLine("HEii");
+            Console.WriteLine("HEii2222222222");
         }
     }
 }
