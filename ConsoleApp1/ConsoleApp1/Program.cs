aab91102dfddafa5502770084c3070add9541d49
DariusGligor <darius.gligor00@e-uvt.ro>
2021-07-24 13:22:22 +0300

    schimba primu

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -11,11 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("HEii");
-<<<<<<< HEAD
-            Console.WriteLine("facem sa fie bine");
-=======
-            Console.WriteLine("HEii");
->>>>>>> fae071290a84c6863832fa4f3e910acfc64d0b61
+            Console.WriteLine("facem sa fie bine111");
         }
     }
 }
